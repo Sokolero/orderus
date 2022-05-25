919f08defae0c8e5eb8d392b86ebd48e7914749b
Sokolero <52373058+Sokolero@users.noreply.github.com>
2022-05-25 16:37:09 +0300

    debug Calendar

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,24 +1,44 @@
 import React, { useState } from "react";
 import styles from "./Calendar.module.css";
 
-// вычислить текущую дату = год, месяц, число
-// получить 6 недель включающих дни этого месяца
-// получить номер дня недели для первого дня
-// получить количество дней приходящих на предыдцщий месяц
-// dayNumber === countFromPrevMonth
+const Day = ({ date }) => {
+  return <div className={styles.day}>{date.getDate() + 1}</div>;
+};
 
-const getSixWeeks = () => {
-  const nowDate = 
-}
+const getBeginDate = () => {
+  const date = new Date();
 
-const Day = ({ day }) => {
-  return <div className={styles.day}>{day}</div>;
+  const nowDate = {
+    day: date.getDate(),
+    month: date.getMonth(),
+    year: date.getFullYear()
+  };
+  const firstDayDate = new Date(nowDate.year, nowDate.month, 1);
+  const firstDayNumber = firstDayDate.getDay();
+  console.log(firstDayNumber);
+  const firstDayDateMs = firstDayDate.getMilliseconds();
+  return new Date(firstDayDateMs - firstDayNumber * 8.64e7);
+};
+
+const getDatesList = () => {
+  const beginDate = getBeginDate();
+  console.log(beginDate.getDate());
+  const dateList = [];
+  dateList.push(beginDate);
+  let currentMsValue = beginDate.getMilliseconds();
+
+  for (let i = 1; i < 42; i += 1) {
+    currentMsValue += 8.64e7;
+    const nextDate = new Date(currentMsValue);
+
+    dateList.push(nextDate);
+  }
+
+  return dateList.map((date, index) => <Day date={date} key={index} />);
 };
 
 const Calendar = ({ employeer = {} }) => {
-  return <div className={styles.calendar}>
-
-  </div>;
+  return <div className={styles.calendar}>{getDatesList()}</div>;
 };
 
 export default Calendar;
